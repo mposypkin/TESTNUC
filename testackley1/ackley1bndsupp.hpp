a64b5cdadcdc70ca904528147f9349308c5f22ae
Alexander Usov <alusov@mail.ru>
2017-01-30 11:01:02 +0300

    Test was updated to use interval analysis new lib

--- testackley1/ackley1bndsupp.hpp
+++ testackley1/ackley1bndsupp.hpp
@@ -16,8 +16,11 @@
 
 #include <math.h>
 #include <box/boxutils.hpp>
-#include <common/interval.hpp>
+#include <interval/interval_air.hpp>
 #include <cutfact/lbcutfact/boundsupp.hpp>
+
+using namespace snowgoose::interval;
+
 
 
 namespace TESTNUC {
@@ -42,38 +45,15 @@ namespace TESTNUC {
          * @return bound
          */
         FT getBound(const snowgoose::Box<FT>& box) {
-            FT lb = 0;
-            FT ub = 0;
+            Interval<FT> t1(0), t2(0);
             for(int i=0; i < mN; i++)
             {
-                FT min, max;
-                snowgoose::Interval<FT>::sqr(box.mA[i], box.mB[i], &min, &max);
-                snowgoose::Interval<FT>::sum(lb, ub, min, max, &lb, &ub);
-            }            
-            snowgoose::Interval<FT>::mult((FT)1/mN, lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::sqrt(lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::mult(-0.2, lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::exp(lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::mult(-20, lb, ub, &lb, &ub);
-            FT templb = lb;
-            FT tempub = ub;
-            
-            lb = 0;
-            ub = 0;
-            for(int i=0; i < mN; i++)
-            {
-                FT min, max;
-                snowgoose::Interval<FT>::mult(2*M_PI, box.mA[i], box.mB[i], &min, &max);                
-                snowgoose::Interval<FT>::cos_(min, max, &min, &max);
-                snowgoose::Interval<FT>::sum(lb, ub, min, max, &lb, &ub);
+                Interval<FT> t(box.mA[i], box.mB[i]);
+                t1 = t1 + sqr(t);
+                t2 = t2 + cos(2*M_PI*t);
             }
-            snowgoose::Interval<FT>::mult((FT)1/mN, lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::exp(lb, ub, &lb, &ub);
-            
-            snowgoose::Interval<FT>::diff(templb, tempub, lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::sum(20, lb, ub, &lb, &ub);
-            snowgoose::Interval<FT>::sum(M_El, lb, ub, &lb, &ub);
-            return lb;
+            auto interval = -20.0 * exp(-0.2 * sqrt(t1/mN)) - exp(t2/mN) + 20 + M_E;
+            return interval.lb();
         }
 
     private:
